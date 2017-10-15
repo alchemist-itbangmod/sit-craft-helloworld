4888ce36a4056833ea7855ded0e1872ef5643964
Pureewat Kaewpoy <pureewat.k@gmail.com>
2017-10-15 17:20:19 +0700

    change text

--- components/HelloWorld.js
+++ components/HelloWorld.js
@@ -6,7 +6,7 @@ export const HelloWorld = ({ text }) => (
 
 class HelloWorldContainer extends React.Component {
   render() {
-    return <HelloWorld text="hello-world" />
+    return <HelloWorld text="hello-world-checkout" />
   }
 }
 
